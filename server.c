9ffafbe07b3dd5302d8fd4d4cc8cd7bef390e0e6
jion <el.manu@gmail.com>
2013-04-11 12:45:36 -0300

    Arreglado login, agregada estetica

--- server.c
+++ server.c
@@ -26,6 +26,8 @@
 #define INCORRECT_PASS_MSG	"Password Erronea."
 #define AUTHENTICATION_OK_MSG	"Autenticacion OK."
 
+const char msg_respuesta[3][50] = { INCORRECT_USER_MSG, INCORRECT_PASS_MSG, AUTHENTICATION_OK_MSG };
+
 void sigchld_handler(int s)
 {
 	while(waitpid(-1, NULL, WNOHANG) > 0);
@@ -41,6 +43,16 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
+/*
+ * La funcion login acepta como entrada dos cadenas, usuario y contrase;a,
+ * y las compara contra el archivo de usuarios definido en el servidor.
+ * 
+ * Devuelve un codigo entero:
+ *  - INCORRECT_USER: Si el usuario pasado como parametro no esta definido.
+ *  - INCORRECT_PASS: Si la contrase;a no se corresponde con la del usuario.
+ *  - AUTHENTICATION_OK: Si el usuario y la contrase;a pasan con exito el
+ *     proceso de autenticacion.
+ */
 int login(const char* user, const char* pass) {
 	int ret= INCORRECT_USER;
 	FILE* fd_auth = fopen("userauth", "r");
@@ -49,7 +61,7 @@ int login(const char* user, const char* pass) {
 	char* fpass;
 	
 	memset(buffer, 0, sizeof(buffer));
-	while ( fgets ( buffer, sizeof buffer, fd_auth ) != NULL ) /* read a line */
+	while ( fgets ( buffer, sizeof buffer, fd_auth ) != NULL )
 	{
 		fuser = strtok(buffer, ";");
 		fpass = strtok(NULL, "\n");
@@ -71,34 +83,45 @@ int login(const char* user, const char* pass) {
 	return ret;
 }
 
-
-/* Esta funcion atiende cada cliente que establece una conexion con nuestro
- * servidor.
+/*
+ * La funcion obtener lee los datos enviados via el socket pasado como argumento
+ * y parsea este flujo para obtener el usuario y la contrase;a que nos envia el
+ * cliente.
  */
 void obtener(int sockfd, char* user, char* pass) {
 	char buffer[LENGTH_BUFFER];
-	char* d[2] = { user, pass };
+	char* d[2] = { user, pass }; // Apunta a la variable a completar
 	char* s; // Apunta a la siguiente caracter a completar de { user, pass }
 	int i, j, numbytes, isParse;
 	
-	j=0;
-	s= d[j];
-	numbytes=1; // Enganio para saltear la primera comprobacion
-	isParse=0;
+	j=0;		// 0-> user; 1-> pass; 2-> todo parseado!
+	s= d[j];	// Primero lo primero (user)
+	numbytes=1;	// Enganio para saltear la primera comprobacion
+	isParse=0;	// Flag de corte para while
 	while(!isParse) {
-		if(numbytes == 0) {
-			perror("Oh my God! An error!");
+		if(numbytes == 0) { // Es decir, esta comprobacion
+			perror("server: Faltan datos de conexion");
+			exit(1);
 		}
-		numbytes = recv(sockfd, buffer,LENGTH_BUFFER,0);
+		numbytes = recv(sockfd, buffer,LENGTH_BUFFER,0); // RECV!
 		if(numbytes < 1) {
-			perror("server: receive");
+			perror("server (receive)");
+			exit(1);
 		}
-
+		
+		/* Se copia la cadena de caracteres desde el buffer a la variable
+		 * correspondiente apuntada por d (user o pass).
+		 */
 		for(i=0; i < numbytes && buffer[i] != '\n'; i++) {
 			*s=buffer[i];
 			s++; // Posicionamos el puntero en la siguiente posicion
 		}
 
+		/* Cuando termina de copiar lo que hay en buffer, verifica si
+		 * ya se envio la cadena completa para la variable actual
+		 * (user o pass). Si es asi, cierra la cadena en la variable
+		 * correspondiente y se pone a trabajar en la siguiente.
+		 */
 		if(i < numbytes) { // Encontramos un '\n' my friend!
 			*s='\0'; // Finalizamos la cadena
 			/* Si existe, quitamos el caracter tabulador */
@@ -107,26 +130,40 @@ void obtener(int sockfd, char* user, char* pass) {
 			j++; /* Pasamos al siguiente "nivel" (pass) */
 			s=d[j];
 		}
-		if(j>1) isParse=1;
+		if(j>1) isParse=1; // Si ya procesamos las dos var, fin.
 	}
 }
 
+/* 
+ * Esta funcion atiende a cada cliente que establece una conexion con nuestro
+ * servidor.
+ */
 void servir(int thread_fd) {
 	char user[MAX_LENGTH_USER];
 	char pass[MAX_LENGTH_PASS];
-	char msg[50];
+	char scode[4];
 	int numbytes;
-	int ret;
+	int code;
 	
 	obtener(thread_fd, user, pass);
 	
-	// printf("DEBUG: Resultado del parse: %s, %s", user, pass);
-	// exit(0);
+//	printf("DEBUG: Resultado del parse: *%s*, *%s*", user, pass);
+//	exit(0);
 	
-	ret = login(user, pass);
+	code = login(user, pass);
 
 	// Devolver el valor que corresponda
-	switch(ret) {
+	sprintf(scode, "%d\n", code);
+	if (send(thread_fd, scode, strlen(scode), 0) == -1) {
+		perror("send: error al responder al cliente");
+		exit(1);
+	}
+	
+	printf("Usuario:  %s\n",user);
+	printf("Password: %s\n",pass);
+	printf("\n%d: %s\n", code, msg_respuesta[code+2]);
+	
+/*	switch(ret) {
 		case  AUTHENTICATION_OK:
 			if (send(thread_fd, "0", 2, 0) == -1) {
 				perror("send");
@@ -152,12 +189,9 @@ void servir(int thread_fd) {
 			sprintf(msg,"Password Erronea");
 			break;
 	}
-	
+*/	
 	close(thread_fd);
 	
-	printf("Usuario:  %s\n",user);
-	printf("Password: %s\n",pass);
-	printf("\n%d: %s\n",ret,msg);
 }
 
 int main(int argc, char *argv[])
